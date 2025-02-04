516a200e25b56b0f40876845313bbb13b79f48fb
Greg Marut <greg.marut@gmail.com>
2025-02-04 15:47:31 -0500

    created a meta model processor for mongodb documents

---
+++ mongodb-sdnmodelgen/src/main/java/com/gregmarut/mongodb/sdn/DocumentMetaModelProcessor.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2024 Greg Marut
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.gregmarut.mongodb.sdn;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Set;
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.TypeElement;
+
+@SupportedAnnotationTypes("org.springframework.data.mongodb.core.mapping.Document")
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+public class DocumentMetaModelProcessor extends AbstractProcessor
+{
+	@Override
+	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv)
+	{
+		for (Element element : roundEnv.getElementsAnnotatedWith(Document.class))
+		{
+			if (element.getKind() == ElementKind.CLASS)
+			{
+				try
+				{
+					processNode((TypeElement) element);
+				}
+				catch (IOException e)
+				{
+					throw new RuntimeException(e);
+				}
+			}
+		}
+		return true;
+	}
+	
+	private void processNode(TypeElement node) throws IOException
+	{
+		// Generate metamodel class for the Node
+		String className = node.getSimpleName() + "_";
+		String packageName = processingEnv.getElementUtils().getPackageOf(node).getQualifiedName().toString();
+		
+		try (Writer writer = processingEnv.getFiler().createSourceFile(packageName + "." + className).openWriter())
+		{
+			writer.write("package " + packageName + ";\n");
+			writer.write("public class " + className + " {\n");
+			processFields(node, writer);
+			writer.write("}\n");
+		}
+	}
+	
+	private void processFields(TypeElement node, Writer writer) throws IOException
+	{
+		// Process superclass fields
+		TypeElement superclass = (TypeElement) processingEnv.getTypeUtils().asElement(node.getSuperclass());
+		if (superclass != null && !superclass.getQualifiedName().toString().equals(Object.class.getName()))
+		{
+			processFields(superclass, writer);
+		}
+		
+		for (Element enclosed : node.getEnclosedElements())
+		{
+			if (enclosed.getKind() == ElementKind.FIELD)
+			{
+				writer.write(
+					"    public static final String " + toUpperSnakeCase(enclosed.getSimpleName().toString()) +
+					" = \"" + enclosed.getSimpleName() + "\";\n");
+			}
+		}
+	}
+	
+	private String toUpperSnakeCase(final String camelCase)
+	{
+		StringBuilder result = new StringBuilder();
+		char[] chars = camelCase.toCharArray();
+		for (char c : chars)
+		{
+			if (Character.isUpperCase(c))
+			{
+				result.append("_");
+			}
+			
+			result.append(Character.toUpperCase(c));
+		}
+		
+		return result.toString();
+	}
+}
